ede3a680445d922c83fb63d09383c1ba30f4e5f6
Deepan <120455127+azhagar-deepan@users.noreply.github.com>
2024-08-07 12:54:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -300,14 +300,14 @@ def admin():
 @app.route("/logout")
 def logout():
     username = session.get("username")
-        if username:
-            try:
-                execute_query('DELETE FROM "user" WHERE username = %s', (username,))
-                session.pop("username", None)
-            except Exception as e:
-                print(f"Error: {e}")
-                return jsonify({"error": "Internal Server Error"}), 500
-        return redirect(url_for("home"))
+    if username:
+        try:
+            execute_query('DELETE FROM "user" WHERE username = %s', (username,))
+            session.pop("username", None)
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": "Internal Server Error"}), 500
+    return redirect(url_for("home"))
 
 @app.route("/graph")
 def graph():
